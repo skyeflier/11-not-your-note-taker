4570938be6c4f57d4cd90cda84ece8783234028a
skyeflier <119133572+skyeflier@users.noreply.github.com>
2023-03-01 22:23:29 -0600

    deleted unnecessary middleware file

--- server.js
+++ server.js
@@ -3,25 +3,16 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util');
 
-// const apiRoute = require('./routes/apiRoutes.js')
-// const apiRoute = require('./routes/htmlRoutes.js')
 const notes = require('./db/db.json')
-
-// const uuid = require('Develop/db/middleware/storage.js'); //This is the middleware that creates the random id number
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "uuid"
-// app.use(storage);
-
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// GET Route for homepage
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
